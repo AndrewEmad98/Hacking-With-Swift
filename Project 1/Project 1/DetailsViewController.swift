ba7de5b93546c77bb0d0f302ef7b34310ef5c8c9
AndrewEmad98 <andrewemad1071998@gmail.com>
2022-07-03 14:35:29 +0200

    complete Project 1

---
+++ Project 1/Project 1/DetailsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailsViewController.swift
+//  Project 1
+//
+//  Created by Andrew Emad on 03/07/2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    var selectedImage : String?
+    var totalImages : Int?
+    var currentImageNumber : Int?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Picture \(currentImageNumber!) of \(totalImages!)"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let loadImage = selectedImage {
+            imageView.image = UIImage(named: loadImage)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+}
