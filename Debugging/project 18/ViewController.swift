a990291b11eb571db687f4720873669b0ed32057
AndrewEmad98 <andrewemad1071998@gmail.com>
2022-07-19 22:37:06 +0200

    complete project 18

---
+++ Debugging/project 18/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  project 18
+//
+//  Created by Andrew Emad on 19/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+//        print("there's error here")
+//        assert(1 == 1 ,"no crashing will happen")
+//        assert(1 == 2 , "there's a crash will be happned")
+        for i in 1...100 {
+            print(i)
+        }
+    }
+
+
+}
+
